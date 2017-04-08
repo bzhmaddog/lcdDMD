933ea3937f08392b3f9fd89a4d8c54a2202b4c1a
Cédric Martin <cedric@ultra.me>
2017-04-08 18:23:43 +0200

    Basic 128x39 dot matrix

--- VideoGen.v
+++ VideoGen.v
@@ -31,33 +31,81 @@ parameter vSyncPorch = 1;
 parameter vSyncLen = 24;
 parameter vFrameSize = 442;
 
-/*
-wire pxSp,clk35;
-
-DCM_SP #(
-      .CLKFX_DIVIDE(2),   // Can be any integer from 1 to 32
-      .CLKFX_MULTIPLY(3) // Can be any integer from 2 to 32
-   ) DCM_SP_inst (
-      .CLKFX(pxSp),   // DCM CLK synthesis out (M/D)
-      .CLKIN(clk),   // Clock input (from IBUFG, BUFG or DCM)
-		.RST(1'b0)
-   );
-BUFG BUFG_pxSp(.I(pxSp), .O(clk35));  // 250MHz
-*/
-
 reg [11:0] CounterX, CounterY;
-always @(posedge clk) CounterX <= (CounterX==hFrameSize-1) ? 12'd0 : CounterX+12'd1;
-always @(posedge clk) if(CounterX==hFrameSize-1) CounterY <= (CounterY==vFrameSize-1) ? 12'd0 : CounterY+12'd1;
+reg [7:0] R, G, B;
+reg [3:0] x_count = 0;
+reg [3:0] y_count = 0;
+reg [7:0] pixel_x = 0;
+reg [5:0] pixel_y = 0;
+
+always @(posedge clk) begin
+ 
+ if (CounterX==hFrameSize-1) begin
+	CounterX <= 12'd0;
+	x_count <= 4'd0;
+	pixel_x <= 8'd0;
+ end else begin
+	CounterX <= CounterX+12'd1;
+  
+   if (x_count >= 9) begin
+		x_count <= 4'd0;
+		pixel_x <= pixel_x+8'd1;
+	end else begin
+		x_count <= x_count+4'd1;
+	end
+	
+ end
+end
+
+always @(posedge clk) begin
+ if (CounterX==hFrameSize-1) begin
+	if (CounterY==vFrameSize-1) begin
+		CounterY <= 12'd0;
+		y_count <= 4'd0;
+		pixel_y <= 6'd0;
+	end else begin
+		CounterY <= CounterY+12'd1;
+		
+		if (y_count >= 9) begin
+			y_count <= 4'd0;
+			pixel_y <= pixel_y+6'd1;
+		end else begin
+			y_count <= y_count+4'd1;
+		end
+	
+	end
+ end
+end
 
 always @(posedge clk) DrawArea <= (CounterX<hDrawArea) && (CounterY<vDrawArea);
 always @(posedge clk) hSync <= (CounterX>=hDrawArea+hSyncPorch) && (CounterX<hDrawArea+hSyncPorch+hSyncLen);
 always @(posedge clk) vSync <= (CounterY>=vDrawArea+vSyncPorch) && (CounterY<vDrawArea+vSyncPorch+vSyncLen);
 
 ////////////////////////////////////////////////////////////////////////
-	reg [7:0] R, G, B;
-	always @(posedge clk) R <= 255;
-	always @(posedge clk) G <= 0;
-	always @(posedge clk) B <= 0;
+	
+	always @(posedge clk) begin
+
+		// draw 2px black border around every 8x8 pixels
+		if (x_count == 0 || x_count == 9 || y_count == 0 || y_count == 9) begin
+			R <= 0;
+			G <= 0;
+			B <= 0;
+		end else begin
+		
+			if (pixel_y == 0 || pixel_y == 38 || pixel_x == 0 || pixel_x == 127) begin
+				R <= 255;
+				G <= 132;
+				B <= 9;
+			end else begin
+				R <= 50;
+				G <= 50;
+				B <= 50;
+			end
+		end
+			
+	end
+
+
 	assign red = R;
 	assign green = G;
 	assign blue = B;
